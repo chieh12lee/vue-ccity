19bfdf37ad8d24010bbb1def4f6d139f04183f47
chieh12lee <chieh.lee@gmail.com>
2025-03-17 22:03:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex relative w-[1920px] h-[1080px] overflow-hidden">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: Welcome,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/Home.vue'),
+    },
+  ],
+})
+
+export default router
